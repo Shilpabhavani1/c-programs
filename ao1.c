b83f145e2e47127aa8b0c7a1c3ad0b6db7682276
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2021-12-23 13:52:09 +0530

    Add files via upload

---
+++ ao1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	int sum,sub,multi,div,mod;
+	printf("enter two numbers");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	sub=a-b;
+	multi=a*b;
+	div=a/b;
+	mod=a%b;
+	printf("sum of two numbers is %d\n",sum);
+	printf("sub of two numbers is %d\n",sub);
+	printf("multi of two numbers is %d\n",multi);
+	printf("div of two numbers is %d\n",div); 
+	printf("mod of two numbers is %d\n",mod);
+	
+	
+	
+}
