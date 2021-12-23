a45f75ee454a166e3d5a74e9b25a406ef7b3ed99
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2021-12-23 14:11:37 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int s,a,p;
+ 
+ 	printf("enter the side of the square");
+ 	scanf("%d",&s);
+ 	a=s*s;
+ 	p=4*s;
+ 	printf("Area of the square of side %d %d",s,a);
+ 	printf("perimeter of the square of side %d %d",s,p);
+ }
